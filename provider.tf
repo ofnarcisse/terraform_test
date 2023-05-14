7d8cab3a50f2e59b3a02e5fe9cd9e1db00105e05
Narcisse <itsupport@live.fr>
2023-05-13 23:49:20 -0600

    new

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+    backend "s3" {
+        bucket = "ofterraform"
+        key = "ofterraform/dev"
+        region = "us-east-1"
+      
+    }
+  
+}
+
 provider "aws" {
     region = var.region
 }
